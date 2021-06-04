eb5d62a45a469b5791478a2035ed5a10f14e0619
lancelee2885 <lancelee2885@gmail.com>
2021-06-03 20:39:43 -0700

    Create fill.js

---
+++ fill.js
@@ -0,0 +1,9 @@
+function fill(arr, value){
+  // add whatever parameters you deem necessary - good luck!
+  let newArr = [];
+  for (i=0; i<arr.length; i++){
+    newArr.push(value);
+  }
+
+  return newArr;
+}
