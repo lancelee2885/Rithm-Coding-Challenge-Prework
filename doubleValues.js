d9b416d210d5a70a4ac55bdb0b39ddccb2db9743
lancelee2885 <lancelee2885@gmail.com>
2021-06-03 20:39:33 -0700

    Create doubleValues.js

---
+++ doubleValues.js
@@ -0,0 +1,8 @@
+function doubleValues(arr){
+  // add whatever parameters you deem necessary - good luck!
+  let result =[];
+  for (i=0; i<arr.length; i++){
+    result.push(arr[i]*2);
+  }
+  return result;
+}
